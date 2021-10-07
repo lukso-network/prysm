c90a2d868a9238526224ca4f38a4ccf366a290b9
mxmar <macht4tech@gmail.com>
2021-10-07 10:53:48 +0200

    Test serve vanguard IPC
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- beacon-chain/rpc/ipc_test.go
+++ beacon-chain/rpc/ipc_test.go
@@ -1,1 +1,25 @@
 package rpc
+
+import (
+	"google.golang.org/grpc"
+	"net"
+	"testing"
+	"time"
+)
+
+func TestServeIpc(t *testing.T) {
+	dialer := func(addr string, t time.Duration) (net.Conn, error) {
+		return net.Dial(protocol, addr)
+	}
+
+	conn, err := grpc.Dial(sockAddr, grpc.WithInsecure(), grpc.WithDialer(dialer))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+
+		}
+	}(conn)
+}
