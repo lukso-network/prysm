0b5b94892cd4998239b688218c0d7d6ef3192529
mxmar <macht4tech@gmail.com>
2021-10-12 17:57:53 +0200

    Provided IPC validator test implementation
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- beacon-chain/rpc/ipc_test.go
+++ beacon-chain/rpc/ipc_test.go
@@ -2,26 +2,52 @@ package rpc
 
 import (
 	"context"
+	"fmt"
+	"go.opencensus.io/plugin/ocgrpc"
+	testgrpc "google.golang.org/grpc/interop/grpc_testing"
+	"google.golang.org/grpc/metadata"
 	"net"
 	"os"
 	"time"
 
-	"github.com/golang/protobuf/proto"
 	"github.com/prysmaticlabs/prysm/shared/testutil/assert"
 	"google.golang.org/grpc"
 
 	"testing"
 )
 
+const defaultTestTimeout = 10 * time.Second
+
+var (
+	// For headers sent to server:
+	testMetadata = metadata.MD{
+		"key1":       []string{"value1"},
+		"key2":       []string{"value2"},
+		"user-agent": []string{fmt.Sprintf("test/0.0.1 grpc-go/%s", grpc.Version)},
+	}
+	// For headers sent from server:
+	testHeaderMetadata = metadata.MD{
+		"hkey1": []string{"headerValue1"},
+		"hkey2": []string{"headerValue2"},
+	}
+	// For trailers sent from server:
+	testTrailerMetadata = metadata.MD{
+		"tkey1": []string{"trailerValue1"},
+		"tkey2": []string{"trailerValue2"},
+	}
+	// The id for which the service handler should return error.
+	errorID int32 = 32202
+)
+
 func TestServeIpc(t *testing.T) {
 	ctx := context.Background()
-	timeout := 5 * time.Second
 
 	service := Service{}
 
-	grpcServerOpts := []grpc.ServerOption{
-		grpc.ChainStreamInterceptor(),
-	}
+	var grpcServerOpts []grpc.ServerOption
+
+	grpcServerOpts = append(grpcServerOpts, grpc.StatsHandler(&ocgrpc.ServerHandler{}))
+
 	service.grpcServer = grpc.NewServer(grpcServerOpts...)
 
 	err := service.serveIpc()
@@ -52,16 +78,30 @@ func TestServeIpc(t *testing.T) {
 	assert.DeepEqual(t, nil, err)
 	assert.NotNil(t, conn)
 
-	t.Run("Check communication", func(t *testing.T) {
-		var req, reply proto.Message
-		ctx, cancel := context.WithTimeout(ctx, timeout)
+	t.Run("doUnaryCall", func(t *testing.T) {
+		var (
+			resp *testgrpc.SimpleResponse
+			req  *testgrpc.SimpleRequest
+			err  error
+		)
+
+		ctx, cancel := context.WithTimeout(ctx, defaultTestTimeout)
+
 		defer cancel()
 
 		assert.NotNil(t, ctx)
 
-		err := conn.Invoke(ctx, "/ethereum.eth.v1alpha1.BeaconChain/GetBeaconConfig", req, reply)
-		if err != nil {
-			t.Fatalf("error is = %v", err)
-		}
+		tc := testgrpc.NewTestServiceClient(conn)
+		assert.NotNil(t, tc)
+		req = &testgrpc.SimpleRequest{Payload: idToPayload(errorID)}
+
+		defer cancel()
+		resp, err = tc.UnaryCall(metadata.NewOutgoingContext(ctx, testMetadata), req, grpc.WaitForReady(false))
+		assert.DeepEqual(t, nil, err)
+		assert.NotNil(t, resp)
 	})
+}
+
+func idToPayload(id int32) *testgrpc.Payload {
+	return &testgrpc.Payload{Body: []byte{byte(id), byte(id >> 8), byte(id >> 16), byte(id >> 24)}}
 }
